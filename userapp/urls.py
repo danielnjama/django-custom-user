03c19dc645ddfc9f1c070ee3922498736b850824
danielnjama <danielnjama2015@gmail.com>
2024-07-05 19:42:04 +0300

    send confirmation code

--- userapp/urls.py
+++ userapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [ 
     path('',views.index,name='index'),
     path("login/",views.login,name="login"),
+    path("activate/",views.activate,name="activate"),
     path("register/",views.register,name="register"),
     path("logout/",views.logout,name="logout"),
+    path("resend/",views.send_activation_code,name="send_activation_code"),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -12,7 +12,7 @@ from .utils import get_activation_code
 
 # Create your views here.
 
-
+@login_required(login_url='login')
 def index(request):
     return render(request,'index.html')
 
@@ -66,20 +66,107 @@ def login(request):
         if request.method == "POST":
             username = request.POST['username']
             password = request.POST['password']
-            if CustomUser.objects.filter(username=username)[0].account_active ==True:
-                User = auth.authenticate(username=username,password=password)
+            
+            User = auth.authenticate(username=username,password=password)
 
-                if User is not None:
+            if User is not None:
+                if CustomUser.objects.filter(username=username)[0].account_active ==True:
                     auth.login(request,User)
                     if request.GET.get('next',None):
                         return HttpResponseRedirect(request.GET['next'])
                     return redirect('/')
-                else:
-                    messages.info(request,'Invalid credentials')
-                    return render(request,'login.html')
-            return render(request,'activate.html')
+                return redirect('activate')
+            else:
+                messages.info(request,'Invalid credentials')
+                return render(request,'login.html')
+            
             
         else:
             return render(request,'login.html')
     else:
         return redirect("/")
+    
+    
+def activate(request):
+    if not request.user.is_authenticated:
+        if request.method =="POST":
+            from .models import CustomUser
+            username = request.POST['username']
+            activation = request.POST['activation']
+            user_instance = CustomUser.objects.filter(username=username)
+            if user_instance and user_instance[0].activation_code == int(activation):
+                user_instance[0].account_active = True
+                user_instance[0].save()
+                return redirect('login')
+            messages.info(request,"Username or code incorect.")  
+            return render(request,'activate.html')        
+                
+                
+        return render(request,'activate.html')
+    return redirect('/')
+
+
+
+# def send_activation_code(request):
+#     if request.method =="POST":
+#         from .models import CustomUser
+#         username = request.POST['username']
+#         user_instance = CustomUser.objects.filter(username=username)
+#         if user_instance.exists():
+#             user = user_instance.first()
+#             activation_code = str(user.activation_code)
+#             subject = "Activation code"
+#             message = f'Hi {username}, kindly use this code to activate your account\nActivation code:{activation_code}'
+#             recipient = user.email
+#             try:
+#                 send_mail(
+#                         subject,
+#                         message,
+#                         'admin@dtechnologies', #sender
+#                         [recipient], #receipient
+#                         fail_silently=False,
+#                     )
+#                 messages.success(request, "Activation code sent successfully.")
+                
+#             except Exception as e:
+#                 return "An error occurred: {}".format(e)
+#         messages.info(request,"Username doesnt exist")  
+#         return render(request,'resend.html')
+#     return render(request,'resend.html')
+
+
+def send_activation_code(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        from .models import CustomUser
+        if username:
+            user_instance = CustomUser.objects.filter(username=username)
+            if user_instance.exists():
+                user = user_instance.first()
+                activation_code = user.activation_code
+                subject = "Activation code"
+                message = f'Hi {username}, kindly use this code to activate your account\nActivation code: {activation_code}'
+                recipient = user.email
+                
+                try:
+                    send_mail(
+                        subject,
+                        message,
+                        'admin@dtechnologies.com',  # sender
+                        [recipient],  # recipient
+                        fail_silently=False,
+                    )
+                    messages.success(request, "Activation code sent successfully.")
+                except Exception as e:
+                    messages.error(request, f"An error occurred while sending the email: {e}")
+            else:
+                messages.info(request, "Username doesn't exist.")
+        else:
+            messages.warning(request, "Please provide a username.")
+        
+        return render(request, 'resend.html')
+    
+    return render(request, 'resend.html')
+                
+        
+        
